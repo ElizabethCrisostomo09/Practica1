0dc96f434aca5b451776a6acdea4d50c7d801d05
Elizabeth <elizabethcrisostomo09@hotmail.com>
2022-10-28 13:56:49 -0500

    errores paractica1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,25 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');  //Se agregó el . 
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+async function displayUser(username) {    //se agrego async
+  $n.textContent = 'cargando..';
+  const response =  await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json()  //se declara la variable
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;  // aqui corregi las comillas
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`  //se agrego el simbolo $
 }
 
 displayUser('stolinski').catch(handleError);
+
+
